4d3cefa0e1cbbd53351f0ec369087d9f724223d8
himanshurajora <iamahalofan@gmail.com>
2022-04-21 11:09:55 +0530

    pizza size selection and static pizza data

--- pizzaman-client/src/components/home/Home.tsx
+++ pizzaman-client/src/components/home/Home.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import './Home.css'
+import { PizzaData } from './PizzaData';
 
 export default function Home() {
+
+    const [pizzaSize, setPizzaSize] = useState<number>(NaN);
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <h1>Customize You Pizza</h1>
+            <section className='section-size'>
+                <h3>Select A Size</h3>
+                <div className='size-container'>
+                    {
+                        PizzaData.sizes.map(size => {
+                            return (
+                                size.id === pizzaSize ?
+                                    <div className='size-item active' key={size.id} onClick={() => setPizzaSize(size.id)}>
+                                        <h4>{size.name}</h4>
+                                        <p>₹ {size.price}</p>
+                                    </div>
+                                    :
+                                    <div className='size-item' key={size.id} onClick={() => setPizzaSize(size.id)}>
+                                        <h4>{size.name}</h4>
+                                        <p>₹ {size.price}</p>
+                                    </div>
+                            )
+
+                        })
+                    }
+                </div>
+            </section>
         </div>
     );
 }
---
+++ pizzaman-client/src/components/home/PizzaData.ts
@@ -0,0 +1,41 @@
+export declare interface PizzaItem{
+    id: number
+    name: string;
+    price: number;
+}
+
+export declare interface PizzaData {
+    sizes : PizzaItem[];
+    crusts : PizzaItem[];
+    toppings : PizzaItem[];
+}
+
+export const PizzaData : PizzaData = {
+    sizes: [
+        {id: 1, name: 'Small - LightWeight', price: 120},
+        {id: 2, name: 'Medium - Best Deal', price: 150},
+        {id: 3, name: 'Large - Heavy Meal', price: 300}
+    ],
+    crusts: [
+        {id: 1, name: 'Thin', price: 10},
+        {id: 2, name: 'Thick', price: 40}
+    ],
+    toppings: [
+        {id: 1, name: 'Cheese', price: 10},
+        {id: 2, name: 'Pepperoni', price: 20},
+        {id: 3, name: 'Mushroom', price: 30},
+        {id: 4, name: 'Onion', price: 20},
+        {id: 5, name: 'Sausage', price: 30},
+        {id: 6, name: 'Pineapple', price: 20},
+        {id: 7, name: 'Ham', price: 30},
+        {id: 8, name: 'Chicken', price: 20},
+        {id: 9, name: 'Bacon', price: 30},
+        {id: 10, name: 'Spinach', price: 20},
+        {id: 11, name: 'Tomato', price: 30},
+        {id: 12, name: 'Olives', price: 20},
+        {id: 13, name: 'Anchovies', price: 30},
+        {id: 14, name: 'Artichoke', price: 20},
+        {id: 15, name: 'Eggplant', price: 30},
+        {id: 16, name: 'Zucchini', price: 20},
+    ]
+}
