cd047b42eaf88c5c643796510dc7e98f6f71a325
himanshurajora <iamahalofan@gmail.com>
2022-04-22 18:40:18 +0530

    ui fixes

--- pizzaman-client/src/components/home/Home.tsx
+++ pizzaman-client/src/components/home/Home.tsx
@@ -11,24 +11,26 @@ export default function Home() {
             <h1>Customize You Pizza</h1>
             <section className='section-size'>
                 <h3>Select A Size</h3>
-                <div className='size-container'>
-                    {
-                        PizzaData.sizes.map(size => {
-                            return (
-                                size.id === pizzaSize ?
-                                    <div className='size-item active' key={size.id} onClick={() => setPizzaSize(size.id)}>
-                                        <h4>{size.name}</h4>
-                                        <p>₹ {size.price}</p>
-                                    </div>
-                                    :
-                                    <div className='size-item' key={size.id} onClick={() => setPizzaSize(size.id)}>
-                                        <h4>{size.name}</h4>
-                                        <p>₹ {size.price}</p>
-                                    </div>
-                            )
+                <div className="section-div">
+                    <div className='size-container'>
+                        {
+                            PizzaData.sizes.map(size => {
+                                return (
+                                    size.id === pizzaSize ?
+                                        <div className='size-item active' key={size.id} onClick={() => setPizzaSize(size.id)}>
+                                            <h4>{size.name}</h4>
+                                            <p>₹ {size.price}</p>
+                                        </div>
+                                        :
+                                        <div className='size-item' key={size.id} onClick={() => setPizzaSize(size.id)}>
+                                            <h4>{size.name}</h4>
+                                            <p>₹ {size.price}</p>
+                                        </div>
+                                )
 
-                        })
-                    }
+                            })
+                        }
+                    </div>
                 </div>
             </section>
         </div>
